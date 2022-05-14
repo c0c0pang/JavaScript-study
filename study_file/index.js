e54e0bcf75536aa8cbe291be9f30f5c2ba601fba
c0c0pang <coc1789@naver.com>
2022-05-14 13:29:27 +0900

    let and const의 특성

---
+++ study_file/index.js
@@ -0,0 +1,16 @@
+console.log("asda")
+
+//variable
+//let은 가변 const는 불변의 특성을 가짐.
+let a=1;
+const name="choi";
+//const로 지정된 변수를 새로운 값으로 할당시 오류가 발생
+
+//object에 들어갈 시 uset는 key가 되고 'choi'는 value가 된다.
+let user='choi'
+//초기 user를 선언했기에 이후에 user의 value값을 바꾸기 위해선 자료형과 같이 선언시 오류가 떠 중복으로 쓸 수 없다
+//그래서 그냥 key만을 이용하여 바꿔야 한다.
+
+let user='kim'
+
+user='kim'
