e9b4fb0d955782f1c131aea65f77a72c51969b71
c0c0pang <coc1789@naver.com>
2022-05-14 22:52:53 +0900

    js의 operators 정리

---
+++ study_file/operators.js
@@ -0,0 +1,11 @@
+let a=1
+let b=2
+// a===b 는 완전일치의 뜻으로 ==보다 상위레벨이다.
+let c=(a===b)
+// ? 는 ternary 연산이며 3>4 ? 에서 앞에 조건이 만족시 true:false 에서 앞의 true를 반환하고 아니라면 false를 반환
+let d= 3>4 ? true:false
+/*
+    && : and
+    || : or
+    !  : not
+*/
